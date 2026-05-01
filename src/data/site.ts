67616226a64c105d3854c76233bf833cdf0e8552
Nick <NHuntley96@gmail.com>
2026-04-30 21:52:16 -0700

    build check

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const siteProfile = {
   name: "Nick Huntley",
   title: "Graphics & Development Portfolio",
-  headline: "Broadcast & Graphics Systems Developer",
+  headline: "Broadcast & Graphics System Developer",
   location: "Irvine, CA",
   bio: "Solutions-Oriented Technical Manager | Broadcast Graphics & Software Integration Expert | NodeJS, .NET & C# Lead Developer | Ross XPression & Adobe Suite Specialist | Unreal Engine Enthusiast",
   portrait: "/images/profile/nick.png",
